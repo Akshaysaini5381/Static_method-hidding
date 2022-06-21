40e663eb69f856f147206272ed839261b6bc394f
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-21 19:40:32 +0530

    Add project files.

---
+++ Static_method hidding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Static_method_hidding
+{
+    // static method hidding :
+    class MyClass
+    {
+        public static void name()
+        {
+            Console.WriteLine("my name akshay saini");
+        }
+        public static void show()
+        {
+            Console.WriteLine("HEllo :");
+        }
+    }
+    class Program:MyClass
+    {
+        public  new static  void show()
+        {
+           Program.name();
+                Console.WriteLine("hiiii :");
+        }
+        static void Main(string[] args)
+        {
+            show();
+
+        }
+    }
+}
